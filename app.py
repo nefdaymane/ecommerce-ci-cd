f06ce73b62ae1f8ced59cf3a2a39bc477dd1ccbf
Aymane Nefdaoui <121317022+nefdaymane@users.noreply.github.com>
2024-11-25 18:28:10 +0100

    add db

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
-# MySQL Configuration
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'ecommerce'
+import os
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', 3306))  # Default to 3306 if not set
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
 mysql = MySQL(app)
 
